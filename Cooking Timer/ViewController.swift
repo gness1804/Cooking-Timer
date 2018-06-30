0ec6482dd2ee2d38b8921e79ebcb54657ccfcc12
gness1804 <graham.nessler@gmail.com>
2018-06-30 16:27:45 -0500

    Finally got timer working.

--- Cooking Timer/ViewController.swift
+++ Cooking Timer/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var timer = Timer()
+    
+    
+    @IBAction func onStopPressed(_ sender: Any) {
+        timer.invalidate()
+        print("Timer stopped.")
+    }
+    
+    @objc func handleTimer() {
+        print("Timer ran.")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.handleTimer), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
